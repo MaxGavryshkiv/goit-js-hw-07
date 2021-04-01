fb0b722c9f441b7cf9ec7839a7cd6324d99cb7e1
Max Gavryshkiv <maks20355@gmail.com>
2021-04-01 14:03:08 +0300

    Відредаговане 5 завдання

--- task-5.js
+++ task-5.js
@@ -6,7 +6,7 @@ let nameOutput = document.getElementById("name-output");
 
  input.oninput = function() {
     if (input.value === ""){
-        nameOutput.innerHTML === "незнакомец"
+        nameOutput.innerHTML = "незнакомец"
     } else {
         nameOutput.innerHTML = input.value
     }
