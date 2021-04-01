05ce6f64598ba3bdc70f9ceff20dd29f2aec39df
Max Gavryshkiv <maks20355@gmail.com>
2021-04-01 14:00:40 +0300

    Відредаговане друге завдання

--- task-2.js
+++ task-2.js
@@ -2,27 +2,16 @@
 // вставит все li за одну операцию в список ul.ingredients.
 //  Для создания DOM-узлов используй document.createElement().
 const ingredients = ["Картошка", "Грибы", "Чеснок", "Помидоры", "Зелень", "Приправы"];
-console.table(ingredients)
-
-
-// const ulRef = document.querySelector('#ingredients');
-const product = ingredient => {
-const liRef = document.createElement('li');
-liRef.textContent = ingredient;
-// ulRef.appendChild(liRef);
-return liRef;
-};
-
-const productCard = ingredients.map(ingredient => product(ingredient));
-
-console.log(productCard);
-
 const productlistRef = document.querySelector('#ingredients');
-console.log(productlistRef);
 
-productlistRef.append(...ingredients);
+const product = ingredients.map(ingredient => {
+    const liRef = document.createElement('li');
+    liRef.textContent = ingredient;
+    return liRef
+    })
+
+productlistRef.append(...product)
+
+console.log(productlistRef)
 
 
-// ingredients.forEach(ingredient => {
-//   console.log(product(ingredient))
-// })
